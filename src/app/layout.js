72b3a0bc05ea3e6e9ffc7b71643117ea3e492540
ozkand-deka <ozkand@dekatechs.com>
2025-01-06 15:55:30 +0300

    Fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 
+import Header from "./components/Header";
 import "./globals.css";
 
 
@@ -7,6 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header/>
         {children}
       </body>
     </html>
